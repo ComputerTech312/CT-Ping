a29b7ced0c104837845a083f1d51472eb9ece6d5
ComputerTech <60820431+ComputerTech312@users.noreply.github.com>
2021-02-10 21:29:24 +0000

    Update ct-ping.tcl

--- ct-ping.tcl
+++ ct-ping.tcl
@@ -10,7 +10,7 @@
 ##########################
 #Set trigger of the Command.
 
-set ctping(trig) ";"
+set ctping(trig) "@"
 
 
 ##################
@@ -62,7 +62,7 @@ set colo "3"
 #Set Time per usage of Command
 ##
 
-set ctping(time) "30"
+set ctping(time) "5"
 
 
 ########################
@@ -72,47 +72,27 @@ set ctping(time) "30"
 bind pub $ctping(flag) $ctping(trig)ping ct:pub:ping
 bind ctcr - PING ct:pingr
 
-proc throttled {id seconds} {
-   global throttle
-   if {[info exists throttle($id)]&&$throttle($id)>[clock seconds]} {
-      set id 1
-   } {
-      set throttle($id) [expr {[clock seconds]+$seconds}]
-      set id 0
-   }
-}
-bind time - ?0* throttledCleanup
-proc throttledCleanup args {
-   global throttle
-   set now [clock seconds]
-   foreach {id time} [array get throttle] {
-      if {$time<=$now} {unset throttle($id)}
-   }
-}
-set pingchan ""
 
-proc pub:ping {nick host hand chan text} {
-  global pingchan pingwho ctping
-      if {[throttled $host,$chan $ctping(time)]} {
-    putserv "NOTICE $nick :Command Throttled"
-    return 
-   }
-  set pingwho [lindex [split $text] 0]
-     if {$pingwho == ""} {set pingwho $nick}
-  putquick "PRIVMSG $pingwho :\001PING [clock clicks -milliseconds]\001"
-  set pingchan $chan
- }
- proc ct:pingr {nick uhost hand dest keyword args} {
-  global pingchan ctping colo pingwho
-  set time [expr {([clock clicks -milliseconds] - $args) / 1000.000}]
-      set char "="
-      if {[expr {round($time / 0.5)}] > 10} {set red 10} else {set red [expr {round($time / 0.5)}]}
-      set green [expr {10 - $red}]
-      set output \00303[string repeat $char $green]\003\00304[string repeat $char $red]\003
-  if {($ctping(msg) == "0")} {
-   putquick "PRIVMSG $pingchan :\[\0030${colo}PING\003\] reply from $pingwho: $output \[\0030${colo}$time\003\] seconds"
-  } else {
-   putquick "NOTICE $nick :\[\0030${colo}PING\003\] reply from $pingwho: \[\0030${colo}$time\003\] seconds"
-  }
- }
+proc ct:pub:ping {nick host hand chan text} {
+global pingchan pingwho
+	set pingwho [lindex [split $text] 0]
+	if {$pingwho == ""} {set pingwho $nick}
+	putquick "PRIVMSG $pingwho :\001PING [clock clicks -milliseconds]\001"
+	set pingchan($pingwho) $chan
+}
+proc ct:pingr {nick uhost hand dest keyword text} {
+	global pingchan ctping colo pingwho
+	set time [expr {([clock clicks -milliseconds] - $text) / 1000.000}]
+	set char "="
+	if {[expr {round($time / 0.5)}] > 10} {set red 10} else {set red [expr {round($time / 0.5)}]}
+	set green [expr {10 - $red}]
+	set output \00303[string repeat $char $green]\003\00304[string repeat $char $red]\003
+	if {($ctping(msg) == "0")} {
+		putquick "PRIVMSG $pingchan($pingwho) :\[\0030${colo}PING\003\] reply from $pingwho: \[\0030${colo}$time\003\] seconds $output"
+	} else {
+		putquick "NOTICE $nick :\[\0030${colo}PING\003\] reply from $pingwho: \[\0030${colo}$time\003\] seconds"
+	}
+unset pingchan
+}
+
 #################################################################################################################################################################
